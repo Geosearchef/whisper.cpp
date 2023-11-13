a3e91d5aa32d4a61722146fd6fe0e568f25285fc
geosearchef <geosearchef@googlemail.com>
2023-11-13 03:01:30 +0100

    input method functionality working

---
+++ examples/whisper.android/app/src/main/java/de/geosearchef/whisperinput/WhisperAccessor.kt
@@ -0,0 +1,62 @@
+package de.geosearchef.whisperinput
+
+import android.app.Application
+import android.widget.Toast
+import com.whispercpp.whisper.WhisperContext
+import com.whispercppdemo.media.decodeWaveFile
+import com.whispercppdemo.recorder.Recorder
+import kotlinx.coroutines.runBlocking
+import java.io.File
+import java.lang.RuntimeException
+import java.nio.file.Paths
+import java.util.concurrent.CompletableFuture
+
+object WhisperAccessor {
+
+    var lastRecording: File? = null
+    val recorder = Recorder()
+
+    var whisperContext: WhisperContext? = null
+
+    fun startRecording() = runBlocking {
+        generateTmpRecordingFile().let { file ->
+            lastRecording = file
+            recorder.startRecording(file, Exception::printStackTrace)
+        }
+    }
+
+    fun stopRecording() = runBlocking {
+        recorder.stopRecording()
+    }
+
+    fun transcribe(language: String = "auto", translate: Boolean = false) : CompletableFuture<String> = CompletableFuture.supplyAsync {
+        val context = whisperContext ?: throw RuntimeException("Model not loaded")
+
+        val audio = lastRecording?.let { decodeWaveFile(it) } ?: throw RuntimeException("Last recording file not found")
+
+        // TODO: do we have to delete the audio again?
+        // TODO: do not write to disk
+
+        // TODO: add cleanup, e.g. context.release()
+        return@supplyAsync runBlocking {
+            val transcription = context.transcribeData(audio, language = language, translate = translate)
+            return@runBlocking transcription
+        }
+    }
+
+    fun loadModelAsync(application: Application, model: Model = Model.DEFAULT) = CompletableFuture.runAsync {
+        whisperContext = WhisperContext.createContextFromAsset(application.assets, "models/" + model.fileName)
+    }
+
+    fun generateTmpRecordingFile() = File.createTempFile("rec", "wav")
+
+    enum class Model(val fileName: String, val label: String = fileName) {
+        TINY("ggml-tiny.bin"), BASE("ggml-base.bin"), SMALL_Q("ggml-small-q5_1.bin"),
+        MEDIUM_Q("ggml-medium-q5_0.bin"),
+        TINY_EN("ggml-tiny.en.bin"), BASE_EN("ggml-base.en.bin"), SMALL_EN_Q("ggml-small.en-q5_1.bin");
+
+        companion object {
+            val DEFAULT = BASE
+        }
+    }
+}
---
+++ examples/whisper.android/app/src/main/java/de/geosearchef/whisperinput/WhisperInputMethodService.kt
@@ -0,0 +1,120 @@
+package de.geosearchef.whisperinput
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.inputmethodservice.InputMethodService
+import android.net.Uri
+import android.provider.Settings
+import android.view.View
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
+import android.view.inputmethod.EditorInfo
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.Toast
+import android.widget.ToggleButton
+import androidx.compose.material3.Text
+import androidx.compose.ui.platform.ComposeView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberPermissionState
+import com.whispercppdemo.R
+import com.whispercppdemo.recorder.Recorder
+import java.nio.file.Paths
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
+
+class WhisperInputMethodService : InputMethodService() {
+
+    lateinit var recordButton: ToggleButton
+    lateinit var computationIndicator: ProgressBar
+    lateinit var modelLoadingIndicator: ProgressBar
+
+    var isRecording: Boolean = false
+
+    @OptIn(ExperimentalPermissionsApi::class)
+    override fun onCreateInputView(): View {
+        val view = layoutInflater.inflate(R.layout.input_method_layout, null)
+
+        recordButton = view.findViewById(R.id.recordButton)
+        computationIndicator = view.findViewById(R.id.computationIndicator)
+        modelLoadingIndicator = view.findViewById(R.id.modelLoadingIndicator)
+
+        recordButton.setOnCheckedChangeListener { buttonView, isChecked ->
+            if(isChecked) {
+                if(requestPermission()) {
+                    isRecording = true
+                    WhisperAccessor.startRecording()
+                } else {
+                    recordButton.isChecked = false
+                }
+            } else if(isRecording) {
+                // stop recording + execute
+                isRecording = false
+                WhisperAccessor.stopRecording()
+
+                // TODO: can this fall through the cracks? wrong state? time out?
+
+                // transcribe
+                transcribe()
+            }
+        }
+
+        // load default model
+//        loadModel()
+        loadModel(WhisperAccessor.Model.BASE)
+
+        return view
+    }
+
+    fun loadModel(model: WhisperAccessor.Model = WhisperAccessor.Model.DEFAULT) {
+        // Load default model
+        modelLoadingIndicator.visibility = VISIBLE
+        WhisperAccessor.loadModelAsync(application, model).thenRun {
+            modelLoadingIndicator.visibility = INVISIBLE
+            println("Model loaded")
+        }
+    }
+
+    fun transcribe() {
+        computationIndicator.visibility = VISIBLE
+        WhisperAccessor.transcribe().thenAccept { transcription ->
+            computationIndicator.visibility = INVISIBLE
+            println(transcription)
+
+            currentInputConnection.commitText(transcription.trim(), 1)
+        }
+    }
+
+
+    fun requestPermission(): Boolean {
+        val permissionGranted = ContextCompat.checkSelfPermission(applicationContext, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED
+        if(!permissionGranted) {
+//            ActivityCompat.requestPermissions()   // requires activity
+            Toast.makeText(applicationContext, "Permission to record Audio not granted.", Toast.LENGTH_LONG).show()
+            Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).let {
+                it.data = Uri.fromParts("package", packageName, null)
+                startActivity(it)
+            }
+        }
+
+        return permissionGranted
+    }
+
+    override fun onStartInputView(editorInfo: EditorInfo?, restarting: Boolean) {
+        // https://developer.android.com/develop/ui/views/touch-and-input/creating-input-method#kotlin
+        super.onStartInputView(editorInfo, restarting)
+    }
+
+    override fun onFinishInput() {
+        super.onFinishInput()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+}
